e5bd1c603ce293439f10df541f4414340cc5ead9
阿QQQ糖 <63430823+13548029769@users.noreply.github.com>
2020-05-07 23:47:37 +0800

    v1.0

--- code/ship.py
+++ code/ship.py
@@ -5,4 +5,14 @@ class Ship():
         """init and set spacecraft position"""
         self.screen = screen
 
-        """loading spacecraft picture and """
+        """loading spacecraft picture and acquire exterior rectangle"""
+        self.image = pygame.image.load("images/ship.bmp")
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+
+        """init spacecraft at bottom center of screen"""
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+
+    def blitme(self):
+        self.screen.blit(self.image,self.rect)
